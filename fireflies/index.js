3f9438f11bdbcbb5c991e75c3d27b3f74438e06d
amar567 <60555078+amar567@users.noreply.github.com>
2022-06-30 04:32:54 +0530

    fireflies initial commit

---
+++ fireflies/index.js
@@ -0,0 +1,54 @@
+let FLY_COUNT = 250;
+let FLY_SPEED = 1;
+let BLINK_FREQUENCY = 0.2;
+let TIME_SPEED_FACTOR = 1;  // how fast time passes [ multiplication factor ]
+let FIELD_OF_VISION = 200;
+let COUPLING_CONSTANT = 0.00001
+
+// initialize the flies
+
+fly_array = [];
+
+for (let i = 0; i < FLY_COUNT; i++) { 
+
+    fly_array.push(
+        new Fly(
+            Math.random() * window.innerWidth, 
+            Math.random() * window.innerHeight, 
+            Math.random() * FLY_SPEED, 
+            Math.random() * 360, 
+            Math.random() > 0.5, 
+            Math.random() * 360
+        )
+    );
+}
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+}
+
+let cycle_timeStamp = new Date().getTime();
+
+function draw() {
+    // set the scene
+    background(51);
+
+    // find the new quad tree
+    fly_array = quadTree(fly_array);
+
+    // update the flies
+    for (let fly of fly_array) {
+        fly.update(fly_array);
+        fly.show();
+    }
+    // console.log(fly_array);
+
+    // render cycle speed
+    let cycle_speed = new Date().getTime() - cycle_timeStamp;
+    let fps = Math.trunc(1000 / cycle_speed);
+    textSize(16);
+    fill(255, 255, 255, 255);
+    text(fps, 10, 16);
+
+    cycle_timeStamp = new Date().getTime();
+}
---
+++ fireflies/quadtree.js
@@ -0,0 +1,5 @@
+const quadTree = (arrayOfFlies)=>{
+    let array = arrayOfFlies
+    // let quadTree = new QuadTree(new Rectangle(0, 0, window.innerWidth, window.innerHeight));
+    return array
+}
